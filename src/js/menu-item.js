c6ce7aeba1b51a8cfd58f92530955bcbe189bb14
Martin Banks <anotherbanksy@gmail.com>
2016-07-22 20:57:46 +1000

    complete lesson 7

--- src/js/menu-item.js
+++ src/js/menu-item.js
@@ -22,13 +22,73 @@ function MenuItem(props){
 }
 */
 
-
+/* // lesson 6
 function MenuItem( {isActive=false} ) {
 	let style = {
 		fontWeight: isActive ? 'bold' : 'normal'
 	}
 	return <li style={style} data-active={isActive}>Menu Item</li>
 }
+*/
+
+
+//lesson 7
+let MenuItem = React.createClass({
+	getDefaultProps: function(){
+		return {
+			isActive: false,
+			message: 'Ooops, not active'	
+		}
+	},
+	getInitialState: function(){
+		return {
+			subMenuVisible: this.props.isActive
+		}
+	},
+	handleClick: function(){
+		this.setState({
+	      subMenuVisible: !this.state.subMenuVisible // toggle
+	    })
+		console.log('click!');
+	},
+	handleHoverOver: function(){
+		this.setState({
+			menuHover: true
+		})
+	},
+	handleHoverOut: function(){
+		this.setState({
+			menuHover: false
+		})
+	},
+
+	render: function(){
+		let style = {
+			fontWeight: this.state.subMenuVisible ? 'bold' : 'normal',
+			color: this.state.menuHover ? 'green' : this.state.subMenuVisible ? 'gold' : 'darkgrey'
+		};
+		let subStyle = {
+			fontStyle: this.state.subMenuVisible ? 'italic' : 'normal',
+			color: this.state.subMenuVisible ? 'lightblue' : 'darkgrey'
+		};
+
+		return <li><span style={style} data-active={this.props.isActive.isActive} onClick={this.handleClick} onMouseOver={this.handleHoverOver} onMouseOut={this.handleHoverOut}>
+			{/*displayMessage()*/}
+			Menu message </span>
+			{this.state.subMenuVisible ? (
+				<p style={subStyle}>
+					Submenu item
+				</p>			
+				) : false 
+			}
+		</li>
+	}
+
+})
+
+
 
 
 module.exports = MenuItem
+
+
